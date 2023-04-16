6db7c45d236ba7faba39ef7a90788e8490e2203e
Internships <84063946+vin-stack@users.noreply.github.com>
2023-04-16 11:51:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,22 @@
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-# Download the 'punkt' corpus
+# Download NLTK data
 nltk.download('punkt')
+nltk.download('stopwords')
+
+# Input text
+text = "This is a sample sentence. It contains some stop words."
+
+# Tokenize the text into words
+words = word_tokenize(text)
+
+# Filter out stop words
+stop_words = set(stopwords.words("english"))
+filtered_words = [word for word in words if word.casefold() not in stop_words]
+
+# Print the original text, tokenized words, and filtered words
+print("Original Text: ", text)
+print("Tokenized Words: ", words)
+print("Filtered Words: ", filtered_words)
